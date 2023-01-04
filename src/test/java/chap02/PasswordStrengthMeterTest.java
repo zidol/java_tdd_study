2f929cf161c343eb08afe46eb38ad4af02290bdd
zidol <ajh3166@naver.com>
2023-01-05 00:09:34 +0900

    두번째 테스트 : 길이만 8글자 미만이고 나머지 조건은 충족하는 경우

--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -15,4 +15,13 @@ public class PasswordStrengthMeterTest {
         PasswordStrength result2 = meter.meter("abc1!Add");
         assertEquals(PasswordStrength.STRONG, result2);
     }
+
+    @Test
+    void meetsOtherCriteria_except_for_Length_Then_Normal(){
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@A");
+        assertEquals(PasswordStrength.NORMAL, result);
+
+    }
+
 }
