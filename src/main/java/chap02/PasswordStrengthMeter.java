71158144cab26b206ffbdc778d70af20e5feefac
zidol <ajh3166@naver.com>
2023-01-04 23:59:46 +0900

    암호 검사기 테스트 코드-  첫번째 테스트 : 모든 규칙을 충족하는 경우 1)컴파일 에러 부터 없앰

---
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -0,0 +1,8 @@
+package chap02;
+
+public class PasswordStrengthMeter {
+
+    public PasswordStrength meter(String s) {
+        return null;
+    }
+}
--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -2,10 +2,14 @@ package chap02;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PasswordStrengthMeterTest {
 
     @Test
-    void name() {
-
+    void meetsAllCriteria_Then_Strong() {
+        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+        PasswordStrength result = meter.meter("ab12!@AB");
+        assertEquals(PasswordStrength.STRONG, result);
     }
 }
