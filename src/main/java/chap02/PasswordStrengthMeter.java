95f80be1a63dbabd4165420ee37e1f6ecf74fe7a
zidol <ajh3166@naver.com>
2023-01-09 23:39:43 +0900

    세번째 테스트 : 숫자를 포함하지 않고 나머지 조건을 충족하는 경우

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -6,6 +6,20 @@ public class PasswordStrengthMeter {
         if (s.length() < 8) {
             return PasswordStrength.NORMAL;
         }
+
+        boolean containsNum = meetsCotainingNumberCriteria(s);
+        if(!containsNum) return PasswordStrength.NORMAL;
+
         return PasswordStrength.STRONG;
+    }
+
+    private boolean meetsCotainingNumberCriteria(String s) {
+        //숫자 포함하는지 체크(0~9)
+        for (char ch : s.toCharArray()) {
+            if (ch >= '0' && ch <= '9') {
+                return true;
+            }
+        }
+        return false;
     }
 }
