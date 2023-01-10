3411d5154bb1abb226ccaee591d2d5aff4eb515f
zidol <ajh3166@naver.com>
2023-01-11 00:22:44 +0900

    일곳 벗째 테스트 : 숫자 포함 조건만 충족하는 경우

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -11,6 +11,9 @@ public class PasswordStrengthMeter {
         if (lengthEnough && !containsNum && !containsUpp) {
             return PasswordStrength.WEAK;
         }
+        if (!lengthEnough && containsNum && !containsUpp) {
+            return PasswordStrength.WEAK;
+        }
         if (!lengthEnough) {
             return PasswordStrength.NORMAL;
         }
--- src/test/java/chap02/PasswordStrengthMeterTest.java
+++ src/test/java/chap02/PasswordStrengthMeterTest.java
@@ -49,4 +49,9 @@ public class PasswordStrengthMeterTest {
         assertStrength("abdefghi", PasswordStrength.WEAK);
     }
 
+    @Test
+    void meetsOnlyNumCriteria_Then_Weak() {
+        assertStrength("12345", PasswordStrength.WEAK);
+    }
+
 }
