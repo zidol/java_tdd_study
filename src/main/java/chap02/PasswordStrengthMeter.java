c9c91773b49609f00da2b9f0d17192aa502db1a9
zidol <ajh3166@naver.com>
2023-01-11 23:39:07 +0900

    meter 메서드 리팩토링

--- src/main/java/chap02/PasswordStrengthMeter.java
+++ src/main/java/chap02/PasswordStrengthMeter.java
@@ -4,24 +4,14 @@ public class PasswordStrengthMeter {
 
     public PasswordStrength meter(String s) {
         if(s == null || s.isEmpty()) return PasswordStrength.INVALID;
-        boolean lengthEnough = s.length() >= 8;
-        boolean containsNum = meetsCotainingNumberCriteria(s);
-        boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        int metCounts = 0;
 
-        if (lengthEnough && !containsNum && !containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-        if (!lengthEnough && containsNum && !containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-        if (!lengthEnough && !containsNum && containsUpp) {
-            return PasswordStrength.WEAK;
-        }
-        if (!lengthEnough) {
-            return PasswordStrength.NORMAL;
-        }
-        if(!containsNum) return PasswordStrength.NORMAL;
-        if (!containsUpp) return PasswordStrength.NORMAL;
+        if(s.length() >= 8) metCounts++;
+        if (meetsCotainingNumberCriteria(s)) metCounts++;
+        if (meetsContainingUppercaseCriteria(s)) metCounts++;
+
+        if (metCounts == 1) return PasswordStrength.WEAK;
+        if (metCounts == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
     }
