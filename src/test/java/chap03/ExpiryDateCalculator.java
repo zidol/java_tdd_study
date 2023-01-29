8ce9ab914359b9489954709ad8f070c2f0fed9a8
zidol <ajh3166@naver.com>
2023-01-29 23:08:07 +0900

    ㄹㅣ팩토링

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -7,19 +7,31 @@ public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDte(PayData payData) {
         int addedMonths = payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            final int datOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
-            if (datOfFirstBilling != candidateExp.getDayOfMonth()) {
-                final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
-                if (dayLenOfCandiMon < datOfFirstBilling) {
-                    return candidateExp.withDayOfMonth(dayLenOfCandiMon);
-                }
-                return candidateExp.withDayOfMonth(datOfFirstBilling);
-            } else {
-                return candidateExp;
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
         } else {
             return payData.getBillingDate().plusMonths(addedMonths);
         }
+    }
+
+    private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+        if (isSameDayOfMonth(candidateExp, payData.getFirstBillingDate())) {
+            final int dayLenOfCandiMon = lastDayOfMonth(candidateExp);
+            final int datOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+            if (dayLenOfCandiMon < datOfFirstBilling) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            return candidateExp.withDayOfMonth(datOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
+    }
+
+    private boolean isSameDayOfMonth(LocalDate candidateExp, LocalDate datOfFirstBilling) {
+        return datOfFirstBilling.getDayOfMonth() != candidateExp.getDayOfMonth();
+    }
+
+    private int lastDayOfMonth(LocalDate candidateExp) {
+        return YearMonth.from(candidateExp).lengthOfMonth();
     }
 }
