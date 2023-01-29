9f0ccf63f7dcb9b4514842aad2990d72d93bbaf1
zidol <ajh3166@naver.com>
2023-01-29 23:12:28 +0900

    10만원 지불에 대한 처리를 추가한 코드 및 테스트

--- src/test/java/chap03/ExpiryDateCalculator.java
+++ src/test/java/chap03/ExpiryDateCalculator.java
@@ -5,7 +5,7 @@ import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDte(PayData payData) {
-        int addedMonths = payData.getPayAmount() / 10_000;
+        int addedMonths = payData.getPayAmount() == 100_000 ? 12 : payData.getPayAmount() / 10_000;
         if (payData.getFirstBillingDate() != null) {
             return expiryDateUsingFirstBillingDate(payData, addedMonths);
         } else {
