8741d1caa7f3b6fe4364ea3a7f48e71a81943b47
zidol <ajh3166@naver.com>
2023-02-22 21:00:04 +0900

    모의 객체로 스텁과 스파이 대체

--- src/test/java/chap07/UserRegisterMockTest.java
+++ src/test/java/chap07/UserRegisterMockTest.java
@@ -1,5 +1,6 @@
 package chap07;
 
+import chap07.exception.WeakPasswordException;
 import chap07.userRegist.EmailNotifier;
 import chap07.userRegist.MemoryUserRepository;
 import chap07.userRegist.UserRegister;
@@ -8,20 +9,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.mock;
 
 public class UserRegisterMockTest {
 
     private UserRegister userRegister;
-    private EmailNotifier mockEmailNotifier = mock(EmailNotifier.class);
     private WeakPasswordChecker mockPasswordChecker = Mockito.mock(WeakPasswordChecker.class);
 
     private MemoryUserRepository fakeRepository = new MemoryUserRepository();
 
+    private EmailNotifier mockEmailNotifier = mock(EmailNotifier.class);
 
     @BeforeEach
     void setUp() {
@@ -30,6 +33,28 @@ public class UserRegisterMockTest {
                 mockEmailNotifier);
     }
 
+    @DisplayName("약한 암호면 가입 실패")
+    @Test
+    void weakPassword() {
+        BDDMockito.given(mockPasswordChecker.checkPasswordWeak("pw")).willReturn(true);
+
+        assertThrows(WeakPasswordException.class, () -> {
+            userRegister.register("id", "pw", "email");
+        });
+    }
+
+    //모의 객체가 기대한 대로 불렸는지 검증하는 코드
+    @DisplayName("회원 가입시 암호 검사 수행함")
+    @Test
+    void checkPassword() {
+        userRegister.register("id", "pw", "email");
+
+        BDDMockito.then(mockPasswordChecker)
+                .should()
+                .checkPasswordWeak(BDDMockito.anyString());
+    }
+
+    //모의 객체의 메서드를 호출할 때 전달한 인자를 구하는 코드
     @DisplayName("가입하면 메일을 전송함")
     @Test
     void whenRegisterThenSendMail() {
