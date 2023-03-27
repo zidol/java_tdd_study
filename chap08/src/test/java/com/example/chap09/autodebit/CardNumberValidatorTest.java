1afe5317bad124d3de6fcbd635bb389e4de22488
zidol <ajh3166@naver.com>
2023-03-28 00:18:31 +0900

    스프링 부트의 내장서버를 이용한 API기능 테스트

--- chap08/src/test/java/com/example/chap09/autodebit/CardNumberValidatorTest.java
+++ chap08/src/test/java/com/example/chap09/autodebit/CardNumberValidatorTest.java
@@ -1,7 +1,12 @@
 package com.example.chap09.autodebit;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardNumberValidatorTest {
