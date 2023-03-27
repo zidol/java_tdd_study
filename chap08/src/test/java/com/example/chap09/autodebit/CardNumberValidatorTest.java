506a15d6dccfeaa36b5cc92cb22ee48e720d4d47
zidol <ajh3166@naver.com>
2023-03-27 23:54:17 +0900

    WireNick을 이용한 Rest 클라이언트 테스ㅡㅌ

---
+++ chap08/src/test/java/com/example/chap09/autodebit/CardNumberValidatorTest.java
@@ -0,0 +1,49 @@
+package com.example.chap09.autodebit;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardNumberValidatorTest {
+
+    private WireMockServer wireMockServer;
+
+    @BeforeEach
+    void setUp() {
+        wireMockServer = new WireMockServer(options().port(8089));
+        wireMockServer.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void valid() {
+        wireMockServer.stubFor(post(urlEqualTo("/card"))
+                .withRequestBody(equalTo("1234567890"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "text/plain")
+                        .withBody("ok"))
+        );
+
+        CardNumberValidator validator =
+                new CardNumberValidator("http://localhost:8089");
+        CardValidity validity = validator.validate("1234567890");
+        assertEquals(CardValidity.VALID, validity);
+    }
+
+    @Test
+    void timeout() {
+        wireMockServer.stubFor(post(urlEqualTo("/card"))
+                .willReturn(aResponse()
+                        .withFixedDelay(5000))
+        );
+
+        CardNumberValidator validator =
+                new CardNumberValidator("http://localhost:8089");
+        CardValidity validity = validator.validate("1234567890");
+        assertEquals(CardValidity.TIMEOUT, validity);
+    }
+}
